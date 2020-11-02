d6cf57cfabf07f74acc35936199512b677a1ad3b
Adam Filipek <adamfilipek1@gmail.com>
2020-11-02 19:28:00 +0100

    Change Atom-iQ color-picker implementation to JSX.
    Save json files with different number of colors (a lot of configurations), for quick switching number of elements in benchmark

--- benchmarks/color-picker/atom-iq/components/App.jsx
+++ benchmarks/color-picker/atom-iq/components/App.jsx
@@ -5,11 +5,10 @@ var rxOperators = require("rxjs/operators");
 var map = rxOperators.map;
 var switchMap = rxOperators.switchMap;
 var distinctUntilChanged = rxOperators.distinctUntilChanged;
-var observeOn = rxOperators.observeOn
 var animationFrameScheduler = require("rxjs").animationFrameScheduler
 var of = require("rxjs").of
 
-module.exports = ({ colors, onMount }) => {
+module.exports = ({colors, onMount}) => {
   const [selectedColorIndex, nextSelectedColorIndex] = createState(0);
   onMount(nextSelectedColorIndex);
 
@@ -17,36 +16,28 @@ module.exports = ({ colors, onMount }) => {
     return of(colors[index].name, animationFrameScheduler)
   })(selectedColorIndex)
 
-  const getClassName = (i) => (source) =>
-    distinctUntilChanged()(map(index => index === i ? "color selected" : "color")(source))
+  const getClassName = (i) => selectedColorIndex.pipe(
+    map(index => index === i ? "color selected" : "color"),
+    distinctUntilChanged()
+  )
 
 
   window.onMount();
 
   return (
-    createRvdElement(1, 'div', 'colors', null, [
-      createRvdElement(1, 'h1', null, null, 'Choose your favorite color:', 3),
-      createRvdElement(1, 'div', 'colors', null, colors.length ? (
-        createRvdElement(1, 'ul', null, null, colors.map(
-          (color, i) => createRvdElement(
-            1,
-            'li',
-            getClassName(i)(selectedColorIndex),
-            {
-              style: { backgroundColor: color.hex },
-              onClick: () => nextSelectedColorIndex(i)
-            },
-            color.name,
-            9
-            )
-        ), 9)
-      ) : (
-        createRvdElement(1, 'div', null, null, 'No colors!', 3)
-      ), 9),
-      createRvdElement(1, 'div', null, null, [
-        'You chose:',
-        createRvdElement(1, 'div', 'chosen-color', null, selectedColorName, 9)
-      ], 6)
-    ], 6)
+    <div class="colors">
+      <h1>Choose your favorite color:</h1>
+      <div class="colors">
+        {colors.length ? (
+          <ul>
+            {colors.map((color, i) => (
+              <li class={getClassName(i)} style={{backgroundColor: color.hex}}
+                  onClick={() => nextSelectedColorIndex(i)}>{color.name}</li>
+            ))}
+          </ul>
+        ) : (<div>No colors!</div>)}
+      </div>
+      <div>You chose: <div class="chosen-color">{selectedColorName}</div></div>
+    </div>
   );
 }
--- benchmarks/color-picker/client.js
+++ benchmarks/color-picker/client.js
@@ -1,4 +1,12 @@
-var colors = require("./colors.json");
+/*
+ * Get colors from json.
+ * Possible values (number of colors), when using "./__colors/_{numberOfColors}.json":
+ * - 133, 266, 532, 1064, 2128, 4256, 8512, 17024, 34048
+ * - 100, 1k, 5k, 10k, 20k, 50k
+ */
+var colors = require("./colors.json"); // 266 colors by default
+// var colors = require("./__colors/_1064.json"); // 1064 colors
+// var colors = require("./__colors/_5k.json"); // 5000 colors
 
 window.registerBenchmark(function(helpers) {
   return {
